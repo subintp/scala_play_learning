8732ec3d0c09d30ac3e9f00a25ee3e6916d18330
Subin T P <subin.tp@truecaller.com>
2020-03-13 17:30:39 +0530

    Query tweet via tweet dao

--- app/dao/TweetDao.scala
+++ app/dao/TweetDao.scala
@@ -6,7 +6,7 @@ import storage.CassandraSession
 @Singleton
 class TweetDao @Inject() (session: CassandraSession)  {
   def findAllTweets: Unit = {
-    val tweets = session.session.execute("select * from killrvideo.videos limit 2").all()
+    val tweets = session.session.execute("select * from twitter.tweets limit 2").all()
     println("Hello Tweets")
     println(tweets)
   }
