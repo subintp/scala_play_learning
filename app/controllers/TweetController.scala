e4a8baf8ad808191d93d0a3b04f1f6124e46685b
Subin T P <subin.tp@truecaller.com>
2020-03-17 17:28:22 +0530

    Add create tweet endpoin as async

--- app/controllers/TweetController.scala
+++ app/controllers/TweetController.scala
@@ -4,11 +4,13 @@ import java.util.UUID
 
 import dao.TweetDao
 import javax.inject._
-import play.api.libs.json.Json
+import play.api.libs.json.{JsValue, Json}
 import play.api.mvc._
 
+import scala.concurrent.{ExecutionContext, Future}
+
 @Singleton
-class TweetController @Inject()(val controllerComponents: ControllerComponents, tweetDao: TweetDao) extends BaseController {
+class TweetController @Inject()(val controllerComponents: ControllerComponents, tweetDao: TweetDao)(implicit ec: ExecutionContext) extends BaseController {
 
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(tweetDao.findAllTweets.toString)
@@ -18,5 +20,12 @@ class TweetController @Inject()(val controllerComponents: ControllerComponents, 
     // Use Option, Some, and None pattern here
     val tweet = tweetDao.find(id)
     Ok(Json.toJson(tweet))
+  }
+
+  def create(): Action[JsValue] = Action.async(parse.json) { implicit request: Request[JsValue] =>
+    val reponseString: Future[String] = Future {
+      tweetDao.findAllTweets.toString
+    }
+    reponseString.map { r => Ok(r) }
   }
 }
