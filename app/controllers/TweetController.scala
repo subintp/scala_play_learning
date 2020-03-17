1ad80cde03222d3e4fe0eae0d1636ae5861a2b8b
Subin T P <subin.tp@truecaller.com>
2020-03-17 17:51:52 +0530

    Add create tweet endpoint

--- app/controllers/TweetController.scala
+++ app/controllers/TweetController.scala
@@ -5,7 +5,7 @@ import java.util.UUID
 import dao.TweetDao
 import javax.inject._
 import models.Tweet
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json._
 import play.api.mvc._
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -26,9 +26,21 @@ class TweetController @Inject()(val controllerComponents: ControllerComponents, 
   }
 
   def create(): Action[JsValue] = Action.async(parse.json) { implicit request: Request[JsValue] =>
-    val responseString: Future[String] = Future {
-      tweetDao.findAllTweets.toString
+    val TweetFromJson: JsResult[Tweet] =
+      Json.fromJson[Tweet](request.body)
+
+    TweetFromJson match {
+      case JsSuccess(t: Tweet, path: JsPath) =>
+        val tweet: Future[Option[Tweet]] = Future {
+          tweetDao.create(t)
+        }
+        tweet.map { t => Ok(Json.toJson(t)) }
+
+      case e @JsError(_) =>
+        val errorResponse: Future[String] = Future {
+          JsError.toJson(e).toString()
+        }
+        errorResponse.map { t => Ok(Json.toJson(t)) }
     }
-    responseString.map { r => Ok(r) }
   }
 }
