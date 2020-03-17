7c48ba48bdb9067322314ab3e73f691bc4fc2002
Subin T P <subin.tp@truecaller.com>
2020-03-17 15:53:34 +0530

    Adds tweet reader

--- app/models/Tweet.scala
+++ app/models/Tweet.scala
@@ -1,19 +1,11 @@
 package models
 
 import java.util.{Date, UUID}
-
-import play.api.libs.json.{Json, Writes}
+import play.api.libs.json._
 
 case class Tweet(user_id: UUID, tweet_id: UUID, created_at: Date = new Date(), content: String)
 
 object Tweet {
-
-  implicit val TweetWrites = new Writes[Tweet] {
-    def writes(tweet: Tweet) = Json.obj(
-      "user_id"  -> tweet.user_id,
-      "tweet_id" -> tweet.tweet_id,
-      "created_at" -> tweet.created_at,
-      "content" -> tweet.content
-    )
-  }
+  implicit val TweetReads = Json.reads[Tweet]
+  implicit val TweetWrites = Json.writes[Tweet]
 }
