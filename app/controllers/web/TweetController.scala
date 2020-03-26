4fa3887c55f2027ba9238bf3a974cc07a10e43ff
Subin T P <subin.tp@truecaller.com>
2020-03-26 20:48:19 +0530

    Restrict tweet index to authenticated user

--- app/controllers/web/TweetController.scala
+++ app/controllers/web/TweetController.scala
@@ -1,5 +1,6 @@
 package controllers.web
 
+import actions.{AuthenticateAction, LoggingAction}
 import dao.TweetDao
 import javax.inject._
 import play.api.mvc._
@@ -7,9 +8,14 @@ import play.api.mvc._
 import scala.concurrent.ExecutionContext
 
 @Singleton
-class TweetController @Inject()(val controllerComponents: ControllerComponents, tweetDao: TweetDao)(implicit ec: ExecutionContext) extends BaseController {
+class TweetController @Inject()(val controllerComponents: ControllerComponents,
+                                tweetDao: TweetDao,
+                                loggingAction: LoggingAction,
+                                authenticate: AuthenticateAction,
+)(implicit ec: ExecutionContext)
+    extends BaseController {
 
-  def index() = Action { implicit request: Request[AnyContent] =>
+  def index() = authenticate { implicit request: Request[AnyContent] =>
     val tweets = tweetDao.findAll
     Ok(views.html.tweet.index(tweets))
   }
